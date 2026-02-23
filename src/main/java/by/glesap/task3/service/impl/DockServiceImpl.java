2b953349429f11e527b9f923cf67e2bca9c4c704
BlackyCGS <glebsapega0307@gmail.com>
2026-02-23 19:51:35 +0300

    DockServiceImpl Changes

--- src/main/java/by/glesap/task3/service/impl/DockServiceImpl.java
+++ src/main/java/by/glesap/task3/service/impl/DockServiceImpl.java
@@ -8,23 +8,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class DockServiceImpl implements DockService {
   private static final Logger logger = LoggerFactory.getLogger(DockServiceImpl.class);
   private final DockRepository repository = DockRepository.getInstance();
+  private final Lock lock = new ReentrantLock();
 
   @Override
   public Dock requestFreeDock() {
     logger.info("Free dock requested");
-    Dock dock = repository.getFreeDock();
-    while (dock == null) {
-      try {
-        TimeUnit.MILLISECONDS.sleep(100);
-      }
-      catch (InterruptedException e) {
-        logger.error(e.getMessage());
-      }
-      dock = repository.getFreeDock();
+    Dock dock;
+    try {
+      lock.lock();
+      do {
+        dock = repository.getFreeDock();
+      } while (dock == null);
+    }
+    finally {
+      lock.unlock();
     }
     return dock;
   }
